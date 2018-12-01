54536d131e93acd77fd616350c6530c3963fc572
Juanita Vyatri <juanita.vyatri@gmail.com>
2018-12-01 15:55:06 +0700

    add Owner file for collectionview cell

---
+++ omdb-example/Views/FilmCell.swift
@@ -0,0 +1,16 @@
+//
+//  FilmCell.swift
+//  omdb-example
+//
+//  Created by Juanita Vyatri on 01/12/18.
+//  Copyright © 2018 Vyatri. All rights reserved.
+//
+
+import UIKit
+
+class FilmCell: UICollectionViewCell {
+    
+    func prepareForReuse() {
+        
+    }
+}
