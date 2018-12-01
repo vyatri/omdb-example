051404929110aa53b93dda64bfaa331d1ded3988
Juanita Vyatri <juanita.vyatri@gmail.com>
2018-12-01 14:09:19 +0700

    fix code indent

--- omdb-example/View Controllers/MasterViewController.swift
+++ omdb-example/View Controllers/MasterViewController.swift
@@ -14,68 +14,68 @@ import UIKit
 
 protocol MasterDisplayLogic: class
 {
-  
+    
 }
 
 class MasterViewController: UIViewController, MasterDisplayLogic, UISearchBarDelegate, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
 {
-  var interactor: MasterBusinessLogic?
-  var router: (NSObjectProtocol & MasterRoutingLogic & MasterDataPassing)?
-
-  // MARK: Object lifecycle
-  
-  override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?)
-  {
-    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-    setup()
-  }
-  
-  required init?(coder aDecoder: NSCoder)
-  {
-    super.init(coder: aDecoder)
-    setup()
-  }
-  
-  // MARK: Setup
-  
-  private func setup()
-  {
-    let viewController = self
-    let interactor = MasterInteractor()
-    let presenter = MasterPresenter()
-    let router = MasterRouter()
-    viewController.interactor = interactor
-    viewController.router = router
-    interactor.presenter = presenter
-    presenter.viewController = viewController
-    router.viewController = viewController
-    router.dataStore = interactor
-  }
-  
-  // MARK: View lifecycle
-  
-  override func viewDidLoad()
-  {
-    super.viewDidLoad()
-    doSomething()
-  }
+    var interactor: MasterBusinessLogic?
+    var router: (NSObjectProtocol & MasterRoutingLogic & MasterDataPassing)?
+    
+    // MARK: Object lifecycle
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?)
+    {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    // MARK: Setup
+    
+    private func setup()
+    {
+        let viewController = self
+        let interactor = MasterInteractor()
+        let presenter = MasterPresenter()
+        let router = MasterRouter()
+        viewController.interactor = interactor
+        viewController.router = router
+        interactor.presenter = presenter
+        presenter.viewController = viewController
+        router.viewController = viewController
+        router.dataStore = interactor
+    }
+    
+    // MARK: View lifecycle
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        doSomething()
+    }
     
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
         self.collectionView.collectionViewLayout.invalidateLayout()
     }
-  
-  // MARK: Do something
-  
+    
+    // MARK: Do something
+    
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var collectionView: UICollectionView!
     
-  
-  func doSomething()
-  {
-    let request = Master.Something.Request()
-    interactor?.doSomething(request: request)
-  }
+    
+    func doSomething()
+    {
+        let request = Master.Something.Request()
+        interactor?.doSomething(request: request)
+    }
     
     
     // card size for list view
@@ -88,11 +88,11 @@ class MasterViewController: UIViewController, MasterDisplayLogic, UISearchBarDel
     // card size for grid view
     func landscapeCellSize() -> CGSize {
         let width:CGFloat = (self.collectionView.bounds.size.width - 60 ) / 4
-         // image ratio is 8:11. Then, 60 height for title is enough
+        // image ratio is 8:11. Then, 60 height for title is enough
         let height:CGFloat = ( width * 11 / 8 ) + 60
         return CGSize(width: width, height: height)
     }
-  
+    
     // MARK: - Searchbar delegate
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
