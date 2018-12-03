38069bf92d8f924d0f48b05356b08144c2fd5412
Juanita Vyatri <juanita.vyatri@gmail.com>
2018-12-03 20:59:00 +0700

    create FilmDetail model

--- omdb-example/Models/FilmDetail.swift
+++ omdb-example/Models/FilmDetail.swift
@@ -10,25 +10,49 @@ import Foundation
 import UIKit
 
 struct FilmDetail: Codable {
-    let imdbID: String
     let Title: String
     let Year: String
-    let Category: CategoryOption
+    let Genre: String
+    let Director: String
+    let Writer: String
+    let Actors: String
+    let Plot: String
+    let Language: String
     let Poster: String
+    let imdbRating: String
+    let imdbVotes: String
+    let imdbID: String
+    let Type: String
     
     private enum CodingKeys: String, CodingKey {
-        case imdbID
         case Title
         case Year
-        case Category = "Type"
+        case Genre
+        case Director
+        case Writer
+        case Actors
+        case Plot
+        case Language
         case Poster
+        case imdbRating
+        case imdbVotes
+        case imdbID
+        case Type
     }
     
-    init(imdbID: String, Title: String, Year: String, Category: CategoryOption, Poster: String) {
-        self.imdbID = imdbID
-        self.Title = Title
-        self.Year = Year
-        self.Category = Category
-        self.Poster = Poster
+    init(data: NSManagedObject) {
+        self.Title = data.value(forKey: "title_") as! String
+        self.Year = data.value(forKey: "year_") as! String
+        self.Genre = data.value(forKey: "genre_") as! String
+        self.Director = data.value(forKey: "director_") as! String
+        self.Writer = data.value(forKey: "writer_") as! String
+        self.Actors = data.value(forKey: "actors_") as! String
+        self.Plot = data.value(forKey: "plot_") as! String
+        self.Language = data.value(forKey: "language_") as! String
+        self.Poster = data.value(forKey: "poster_") as! String
+        self.imdbRating = data.value(forKey: "imdbRating_") as! String
+        self.imdbVotes = data.value(forKey: "imdbVotes_") as! String
+        self.imdbID = data.value(forKey: "imdbID") as! String
+        self.Type = data.value(forKey: "type_") as! String
     }
 }
